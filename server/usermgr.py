720d2a8d89e4d1e9d592ffbb633c637dbe622333
Marionumber1 <gksharkboy@gmail.com>
2015-01-10 14:01:23 -0500

    Started usermgr

--- server/usermgr.py
+++ server/usermgr.py
@@ -1,3 +1,12 @@
+import os, hashlib
+
 # Attempt to authenticate a user, returning an access token
 def login(username, password):
-    return None
+    # Look up the user in the database
+
+    # Hash the password (along with the salt)
+    sha512 = hashlib.sha512()
+    sha512.update(bytes(password))
+    password_hash = sha512.digest()
+
+    # Verify the user's hash
