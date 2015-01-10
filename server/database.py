5006f53040c67cc501bb9f7ea13fd8b1f88ebba7
Marionumber1 <gksharkboy@gmail.com>
2015-01-10 15:21:39 -0500

    Written usermgr.init()

--- server/database.py
+++ server/database.py
@@ -2,16 +2,20 @@
 import dataset
 import json
 
+# Database
 db = None
 
-
-def db_init():
+def init():
     """Initialize the database"""
     global db
     conf = json.loads(open("db.json").read())
     db = dataset.connect(conf["db"])
 
-
-def db_get_table(name):
+def get_table(name):
+    """Get a table from the database"""
     return db.get_table(name)
+
+def add_table(name):
+    """Add a new table to the database"""
+    return db.create_table(name)
 
