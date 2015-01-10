24e58ed8d9540e54eb00284dbfaed7a9a5b55ee3
Marionumber1 <gksharkboy@gmail.com>
2015-01-10 14:13:49 -0500

    Moved server code to server.py

--- server/database.py
+++ server/database.py
@@ -1,5 +1,10 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 import sqlite3
 import dataset
 conn = sqlite3.connect('database.db')
 db = dataset.connect('sqlite:///database.db')
+
+# Initialize the database
+def db_init():
+    pass
+
--- server/main.py
+++ server/main.py
@@ -1,44 +1,5 @@
-from flask import Flask, session, redirect, url_for, escape, request
-
-import usermgr
-
-app = Flask(__name__)
-
-# Root URL
-@app.route("/")
-def index():
-    # Already logged in
-    if "token" in session:
-        return redirect(url_for("teams"))
-    # Not logged in
-    else:
-        return redirect(url_for("login"))
-
-# Login page
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    # Sending credentials
-    if request.method == "POST":
-        # Get the username and password from the webform
-        username = request.form["username"]
-        password = request.form["password"]
-
-        # Attempt to login
-        token = usermgr.login(username, password)
-
-        # Give them access if the password check succeeded
-        if token:
-            session["token"] = token
-            return redirect(url_for("index"))
-
-    # GET request or authentication failure
-    return open("login.html").read()
-
-# Logout page
-@app.route("/logout")
-def logout():
-    session.pop("token", None)
-    return redirect(url_for("index"))
+import database, server
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9001)
+    database.db_init()
+    server.server_init()
