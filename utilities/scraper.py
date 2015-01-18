888e68fd143a09b73985f119085c074c34311eec
Antares C <antares21chen@gmail.com>
2015-01-18 16:43:48 -0500

    Finished scraper. Did not test. May not work.

--- utilities/scraper.py
+++ utilities/scraper.py
@@ -11,16 +11,22 @@ import requests
 # Constants
 DESCRIPTION = "Retrieves data from the Blue Alliance API and stores it in the Scoutmaster 9001 database."
 EPILOG = "(c) 2015 Noah Kim, Antares Chen; Team 449"
-
-EVENTS_BY_YEAR = "http://www.thebluealliance.com/api/v2/events/%s"
-TEAMS_BY_NUMBER = "http://www.thebluealliance.com/api/v2/team/frc%s"
-TEAMS_BY_EVENT = "http://www.thebluealliance.com/api/v2/event/%s/teams"
 REQUEST_HEADERS = {"X-TBA-App-Id": "frc449:scoutmaster:v02"}
+
+DEFAULT_TEAM = "http://www.thebluealliance.com/api/v2/team/frc%s"
+EVENT_MATCHES_BY_TEAM = "http://www.thebluealliance.com/api/v2/team/%s/event/%s/matches"
+
+DEFAULT_EVENT = "http://www.thebluealliance.com/api/v2/event/%s"
+DEFAULT_TEAMS_BY_EVENT = "http://www.thebluealliance.com/api/v2/event/%s/teams"
+# Variable below is redundant
+SPECIFIED_TEAMS_BY_EVENT = "http://www.thebluealliance.com/api/v2/team/%s/event/%s/matches"
+MATCHES_BY_EVENT = "http://www.thebluealliance.com/api/v2/event/%s/matches"
+RANKING_BY_EVENT = "http://www.thebluealliance.com/api/v2/event/%s/rankings"
 
 
 # Scraper function
 def scrape(key, url):
-	response = requests.get(url%str(key), headers=REQUEST_HEADERS)
+	response = requests.get(url%key, headers=REQUEST_HEADERS)
 	json_data = json.loads(response.text)
 	# with open("output.json", "w") as f:
 	# 	f.write(json.dumps(json_data))
@@ -28,27 +34,48 @@ def scrape(key, url):
 
 # Parser settings
 parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
-parser.add_argument("-y", "--year", help="Year to retrieve event list from", metavar="<year #>", nargs="*", type=int)
-parser.add_argument("-t", "--team", help="Team number to retrieve data from", metavar="<team #>", nargs="*", type=int)
-parser.add_argument("-e", "--event", help="Event to retrieve teams' data from", metavar="\"<event key>\"", nargs="*", type=str)
+parser.add_argument("target", 
+					help="Specify the target of the scraper. If an alpha-numeric key is given then the program will target a regional, else it will target a team.",  
+					type=str)
+parser.add_argument("-e", "--event", 
+					help="Scrape the team for its participated events.",  
+					nargs="+", 
+					type=str)
+parser.add_argument("-t", "--team", 
+					help="Scrape the event for its participating teams.",
+					nargs="*", 
+					type=str)
+parser.add_argument("-m", "--match", 
+					help="Scrape all matches from the target event", 
+					action="store_true")
+parser.add_argument("-r", "--ranking", 
+					help="Scrape the ranking of teams at the target event",
+					action="store_true")
+
 parser.add_argument("-o", "--overwrite", help="Overwrite previous team data", action="store_true")
 args = parser.parse_args()
 
 
-# if args.team and args.event:
-#     print("You cannot look up a team and a region at the same time!", file=sys.stderr)
-# elif not (args.team or args.event):
-#     print("Please specify what to look up!", file=sys.stderr)
+# If target is numeric then it is a team code, else event code
+if args.target.isnumeric():	
+	if args.event:
+		for event in args.event:
+			print(scrape((args.target, event), EVENT_MATCHES_BY_TEAM))
+	else:
+		print(scrape(args.target, DEFAULT_TEAM))
+else:
+	if args.team:
+		if len(args.team) == 0:
+			print(scrape(args.target, DEFAULT_TEAMS_BY_EVENT))
+		else:
+			for team in args.team:
+				print(scrape((args.target, team), SPECIFIED_TEAMS_BY_EVENT))
+	elif args.match:
+		print(scrape(args.target, MATCHES_BY_EVENT))
+	elif args.ranking:
+		print(scrape(args.target, RANKING_BY_EVENT))
+	else:
+		print(scrape(args.target, DEFAULT_EVENT))
 
-# More parser settings
-if args.year:
-	for year in args.year:
-		print(scrape(year, EVENTS_BY_YEAR))
-if args.team:
-	for team in args.team:
-	    print(scrape(team, TEAMS_BY_NUMBER))
-if args.event:
-	for event in args.event:
-		print(scrape(event, TEAMS_BY_EVENT))
 
 
