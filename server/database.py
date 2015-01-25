973f9aca6f1c35b6d8ee9268f57b1fb6f9ae099e
Antares C <antares21chen@gmail.com>
2015-01-25 11:54:21 -0500

    Added a bunch of things

--- server/database.py
+++ server/database.py
@@ -2,29 +2,65 @@
 import pymongo
 import conf
 
-# Database
-db = None
+# Mongo client
+client = None
 
 
-class TableNotFoundException(Exception):
+class DatabaseNotFoundException(Exception):
+	pass
+
+
+class CollectionNotFoundException(Exception):
     pass
 
 
-def get_table(name):
+class DocumentNotFoundException(Exception):
+	pass
+
+
+def get_database(name):
+	"""Get a database from mongodb"""
+	if name in client.database_names():
+		return client[name]
+	raise DatabaseNotFoundException
+
+
+def add_database(name):
+	"""Add a database to mongodb"""
+	return client[name]
+
+
+def get_collection(db, name):
     """Get a table from the database"""
-    if name in db.collection_names:
-        return pymongo.Collection(db, name)
-    raise TableNotFoundException
+    if name in client[db].collection_names():
+        return client[db][name]
+    raise CollectionNotFoundException
 
 
-def add_table(name):
+def add_collection(db, name):
     """Add a new table to the database"""
-    return pymongo.Collection(db, name)
+    return client[db][name]
+
+
+def get_document(db, collection, key):
+	"""Get a document from the collection"""
+	result = client[db][collection].find_one(key)
+	if result != None:
+		return result
+	raise DocumentNotFoundException
+
+
+def add_document(db, collection, item):
+	"""Add a document to the collection"""
+	# Make sure that the item being added to the collection matches the schema of the collection 
+	
+
+
 
 
 def init():
     """Initialize the database"""
-    global db
-    conn = pymongo.MongoClient("localhost", 9002)
-    db = conn[conf.lookup("db")]
+    global client
+    client = pymongo.MongoClient(conf.lookup("db"))
+    
 
